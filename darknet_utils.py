9ab9419e0e3d4956867d01d131e5ea7fdc2f7a0b
Willie Maddox <willie.maddox@gmail.com>
2017-04-22 21:26:37 -0500

    add code to prepare data for darknet training/testing

---
+++ darknet_utils.py
@@ -0,0 +1,34 @@
+
+import os
+import numpy as np
+src_dir = '/media/RED6/DATA/geos/cowc/datasets'
+patch_dir = os.path.join(src_dir, 'patch_sets/detection')
+truth_dir = os.path.join(src_dir, 'ground_truth_sets')
+dst_dir = '/home/maddoxw/temp/cowc'
+anno_dir = os.path.join(dst_dir, 'annotations')
+
+img_files = []
+for path, _, files in os.walk(anno_dir):
+    for file in files:
+        if file.endswith('txt'):
+            continue
+        img_files.append(os.path.join(path, file))
+
+np.random.shuffle(img_files)
+
+n_splits = 5
+n_split = 1
+n_files = len(img_files)
+n_test = n_files / n_splits + 1
+i_files = n_test
+test_dict = {i+1: [] for i in range(n_splits)}
+for ii, img_file in enumerate(img_files):
+    if ii >= i_files:
+        n_split += 1
+        i_files += n_test
+    test_dict[n_split].append(img_file)
+
+for n_split, test_list in test_dict.iteritems():
+    with open(os.path.join(dst_dir, 'test' + str(n_split) + '.txt'), 'w') as ofs:
+        for test_file in test_list:
+            ofs.write(test_file + '\n')
